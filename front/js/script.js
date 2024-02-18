3e520eade3c188dec72dd38d393dfb486f2757c0
Jessica Williams <walktherosepath@gmail.com>
2024-02-18 13:20:59 -0700

    created new script file and mapped out notes of what to do next

---
+++ front/js/script.js
@@ -0,0 +1,10 @@
+console.log("Testing...");
+
+//TODO Get the array from the backend API
+//TODO Get the existing element on the page where I can insert the cards (look in section tag)
+//TODO Iterate over the array that I got from the backend
+//      AND get the current element in the array
+//      AND create a new DOM element to be inserted into the homepage
+//      AND insert current element's info into the new DOM element
+//      AND append (child) new DOM element into existing section tag element on page
+
